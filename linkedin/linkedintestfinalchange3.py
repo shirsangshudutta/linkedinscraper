1b6877d6316681271fc21f1783b441e0e7e2d84e
Shirsangshu <shirsangshudutta@gmail.com>
2021-04-13 00:07:31 +0530

    commit

--- linkedin/linkedintestfinalchange3.py
+++ linkedin/linkedintestfinalchange3.py
@@ -143,10 +143,10 @@ def visitProfile(link):
                 print('university',university)
                 print('branch',branch)
                 if(t.find("h3",{"class":"pv-entity__school-name t-16 t-black t-bold"}).find(text=re.compile("Jadavpur University",re.IGNORECASE))) :
-                    thisdict['college']=t.find("h3",{"class":"pv-entity__school-name t-16 t-black t-bold"}).text if t.find("h3",{"class":"pv-entity__school-name t-16 t-black t-bold"}) else '',
-                    thisdict['degree']=t.find("p",{"class":"pv-entity__secondary-title pv-entity__degree-name t-14 t-black t-normal"}).find("span",{"class":"pv-entity__comma-item"}).text if t.find("p",{"class":"pv-entity__secondary-title pv-entity__degree-name t-14 t-black t-normal"}) else '',
+                    thisdict['college']=t.find("h3",{"class":"pv-entity__school-name t-16 t-black t-bold"}).text if t.find("h3",{"class":"pv-entity__school-name t-16 t-black t-bold"}) else ''
+                    thisdict['degree']=t.find("p",{"class":"pv-entity__secondary-title pv-entity__degree-name t-14 t-black t-normal"}).find("span",{"class":"pv-entity__comma-item"}).text if t.find("p",{"class":"pv-entity__secondary-title pv-entity__degree-name t-14 t-black t-normal"}) else ''
                     # thisdict['branch']=clean_string(t.find("p",{"class":"pv-entity__secondary-title pv-entity__fos t-14 t-black t-normal"}).find("span",{"class":"pv-entity__comma-item"}).text) if t.find("p",{"class":"pv-entity__secondary-title pv-entity__fos t-14 t-black t-normal"}).find("span",{"class":"pv-entity__comma-item"}).find(text=re.compile("Computer Science",re.IGNORECASE)) else '',
-                    thisdict['branch']=clean_string(t.find("p",{"class":"pv-entity__secondary-title pv-entity__fos t-14 t-black t-normal"}).find("span",{"class":"pv-entity__comma-item"}).text) if t.find("p",{"class":"pv-entity__secondary-title pv-entity__fos t-14 t-black t-normal"}).find("span",{"class":"pv-entity__comma-item"}).find(text=re.compile("Computer Science",re.IGNORECASE)) else '',
+                    thisdict['branch']=clean_string(t.find("p",{"class":"pv-entity__secondary-title pv-entity__fos t-14 t-black t-normal"}).find("span",{"class":"pv-entity__comma-item"}).text) if t.find("p",{"class":"pv-entity__secondary-title pv-entity__fos t-14 t-black t-normal"}).find("span",{"class":"pv-entity__comma-item"}).find(text=re.compile("Computer Science",re.IGNORECASE)) else ''
                     thisdict['duration']=clean_string(t.find("p",{"class":"pv-entity__dates t-14 t-black--light t-normal"}).find("span",{"class":""}).text) if t.find("p",{"class":"pv-entity__dates t-14 t-black--light t-normal"}).find("span",{"class":""}) else ''
                                 
         # for x, y in thisdict.items():
@@ -160,9 +160,9 @@ def visitProfile(link):
             print("@@@@here@@@@@")
             for k in html_soup.find_all("div",{"class":"pv-entity__summary-info pv-entity__summary-info--background-section mb2"},limit=2) :
                     print("####there@@@@@")
-                    thisdict['designation']= clean_string(k.find("h3",{"class":"t-16 t-black t-bold"}).text) if k.find("h3",{"class":"t-16 t-black t-bold"}) else '',
-                    thisdict['company']=clean_string(k.find("p",{"class":"pv-entity__secondary-title t-14 t-black t-normal"}).text) if k.find("p",{"class":"pv-entity__secondary-title t-14 t-black t-normal"}) else '',
-                    thisdict['employ_duration'] =clean_string(k.find("span",{"class":"pv-entity__bullet-item-v2"}).text) if k.find("span",{"class":"pv-entity__bullet-item-v2"}) else '',
+                    thisdict['designation']= clean_string(k.find("h3",{"class":"t-16 t-black t-bold"}).text) if k.find("h3",{"class":"t-16 t-black t-bold"}) else ''
+                    thisdict['company']=clean_string(k.find("p",{"class":"pv-entity__secondary-title t-14 t-black t-normal"}).text) if k.find("p",{"class":"pv-entity__secondary-title t-14 t-black t-normal"}) else ''
+                    thisdict['employ_duration'] =clean_string(k.find("span",{"class":"pv-entity__bullet-item-v2"}).text) if k.find("span",{"class":"pv-entity__bullet-item-v2"}) else ''
                     thisdict['dates_employed'] =clean_string(k.find("h4",{"class":"pv-entity__date-range t-14 t-black--light t-normal"}).find("span",{"class":""}).text) if k.find("h4",{"class":"pv-entity__date-range t-14 t-black--light t-normal"}) else ''
                                     # i=i+1                  
                         # for x, y in thisdict.items():
@@ -184,8 +184,8 @@ def visitProfile(link):
     return thisdict 
 #%%
 # from selenium.webdriver.common.action_chains import ActionChains
-link='https://www.linkedin.com/in/shanawaz-alam-ju/'
-# link='https://www.linkedin.com/in/chandreyee-chowdhury-56244263/'
+# link='https://www.linkedin.com/in/shanawaz-alam-ju/'
+link='https://www.linkedin.com/in/chandreyee-chowdhury-56244263/'
 # link='https://www.linkedin.com/in/arjun-murmu-82073b159/'
 Recdict=visitProfile(link)
 for x, y in Recdict.items():
@@ -221,7 +221,7 @@ profilesQueued = []
 profilesID = []
 page = 0
 index = 0
-limit = 20
+limit = 10
 ids1 = []
 # Defaults1 = {'Name': '', 'Edu': '', 'Exp': ''}
 IDs = []
@@ -230,7 +230,7 @@ Defaults1 = {'Name':'','Link':'','College': '', 'Degree': '', 'Branch': '', 'dur
 Recdict = dict.fromkeys(ids1, Defaults1)
 baselink = browser.current_url.partition('page')[0]
 print('@@@@@@@@@@@@@baselink@@@@@@@@@@@@@@@@@@',baselink)
-while(page <5):
+while(page <3):
     if page==0:
         nextlink=baselink
         print('page 0 nextlink:'+nextlink)
@@ -284,14 +284,25 @@ print (len(Recdict))
 # print(Recdict)
 for x, y in Recdict.items():
     print(x, y)  
-
+#%%
+print (len(Recdict))
+# print(Recdict)
+for x, y in Recdict.items():
+    print(x, y)  
 
 #%%
  #print only values having non null
+import bson
 recdict1={k:v for k,v in Recdict.items() if  v}
 for x, y in recdict1.items():
-    print(x,y)  
-#%%
+    print(x,y) 
+print (len(Recdict))
+# print(Recdict)
+values=Recdict.values()
+values_list = list(values);
+print(values_list)
+
+    #%%
 import bson
 from bson import json_util
 Recdict1=Recdict.values()
@@ -338,19 +349,17 @@ myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["mydatabase"]
 mycol = mydb["alumni"]
 mycol.delete_many({})
-new_rectdict = {k:v for k,v in Recdict.items() if v}
-for x,y in new_rectdict.items():
-   print(y) 
-   print(type(y))
-   z=mycol.insert_one(y)
-#    bson_ex=BSON.encode(x,y)
-#    print(type(bson_ex))
-   
-
-#    z=mycol.insert_one(y)
+x = mycol.insert_many(values_list)
+print('ids of inserted documents\n---------------------')
+for id in x.inserted_ids:
+	print(id)
+#%%
+for doc in mycol.find():
+    #Print each document
+    print(doc)
 # %% 
 print(z)
-myquery = {"Name": "ARJUN MURMU" }  
+myquery = {"Name": "Chandreyee Chowdhury" }  
 a = mycol.find(myquery)   
 for q in a:
   print(q)    
