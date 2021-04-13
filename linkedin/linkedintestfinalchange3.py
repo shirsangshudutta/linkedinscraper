b8afe2c81812e35d756a5ae0d8ce15bd5ddb55f8
Shirsangshu <shirsangshudutta@gmail.com>
2021-04-14 00:53:20 +0530

    commited 14th

--- linkedin/linkedintestfinalchange3.py
+++ linkedin/linkedintestfinalchange3.py
@@ -60,8 +60,15 @@ que.send_keys("jadavpur university department of information technology")
 que.send_keys(Keys.ENTER)
 # time.sleep(random.randint(1, 5))
 # link2=browser.current_url
-# print('link2@@@@'+link2)
-
+# print('link2@@@@'+link2)3
+#%%
+from selenium.webdriver import ActionChains
+browser.get("https://www.linkedin.com/in/arjun-murmu-82073b159/")
+# identify element
+# l=browser.("//*[@id="ember125"]/div[2]/div/h3")
+# # action object creation to scroll
+# a = ActionChains(driver)
+# a.move_to_element(l).perform()
 
 # %%
 from selenium.webdriver.common.action_chains import ActionChains
@@ -88,9 +95,9 @@ def visitProfile(link):
     # element = browser.find_elements_by_css_selector('#education-section')
     # actions = ActionChains(browser)
     # actions.move_to_element(element).perform()
-    browser.execute_script("window.scrollTo(0,document.body.scrollHeight)")
     html_soup = BeautifulSoup(browser.page_source,'html.parser')
     print('link',browser.current_url)
+    # browser.execute_script("window.scrollTo(0,document.body.scrollHeight)")
     # browser.execute_script("window.scrollTo(0,1200)")
 
     # name = html_soup.find("li", {"class": "inline t-24 t-black t-normal break-words"})
@@ -118,37 +125,35 @@ def visitProfile(link):
     # print(test1)
     # element = browser.find_elements_by_css_selector('#ember140 > div.pv-entity__summary-info.pv-entity__summary-info--background-section > div > h3')
     # browser.execute_script("arguments[0].scrollIntoView();", element)
-    # test=html_soup.find("h3",{"class":"pv-entity__school-name t-16 t-black t-bold"}).find(text=re.compile("Jadavpur University",re.IGNORECASE))
-    # print(test)
-    try:
-
-    # if(html_soup.find("h3",{"class":"pv-entity__school-name t-16 t-black t-bold"}).find(text=re.compile("Jadavpur University",re.IGNORECASE))) is not None:
+   
+    # if (html_soup.find("h3",{"class":"pv-entity__school-name t-16 t-black t-bold"}).find(text=re.compile("Jadavpur University",re.IGNORECASE))) is not None:
     #         print("@@@@edu@@@@")
     #         if (html_soup.find("p",{"class":"pv-entity__secondary-title pv-entity__fos t-14 t-black t-normal"}).find("span",{"class":"pv-entity__comma-item"}).find(text=re.compile("Computer Science",re.IGNORECASE))) is not None:
-    #             print("@@@@infotech@@@@")
-        name = html_soup.find("li", {"class": "inline t-24 t-black t-normal break-words"})
-        if name is not None:
-            print(name.text)
-        IDs = []
-        # Defaults = {'Name':'','College': '', 'Degree': '', 'Branch': '', 'duration': '','designation': '', 'company': '','dates_employed': '', 'employ_duration': ''}
-        # thisdict = dict.fromkeys(IDs, Defaults)
-        thisdict['Name']= clean_string(name.text)
-        thisdict['Link']= clean_string(link)
-        time.sleep(random.randint(1,5))
-        # window.scrollTo(0,document.body.scrollHeight)
-        if (html_soup.find("section", {"id":"education-section"})):
-            print('section1')
-            edusect=html_soup.find("section", {"class":"pv-profile-section education-section ember-view"})
-            for t in edusect.find_all("li", {"class": "pv-profile-section__list-item pv-education-entity pv-profile-section__card-item ember-view"}):
-                print('university',university)
-                print('branch',branch)
-                if(t.find("h3",{"class":"pv-entity__school-name t-16 t-black t-bold"}).find(text=re.compile("Jadavpur University",re.IGNORECASE))) :
-                    thisdict['college']=t.find("h3",{"class":"pv-entity__school-name t-16 t-black t-bold"}).text if t.find("h3",{"class":"pv-entity__school-name t-16 t-black t-bold"}) else ''
-                    thisdict['degree']=t.find("p",{"class":"pv-entity__secondary-title pv-entity__degree-name t-14 t-black t-normal"}).find("span",{"class":"pv-entity__comma-item"}).text if t.find("p",{"class":"pv-entity__secondary-title pv-entity__degree-name t-14 t-black t-normal"}) else ''
-                    # thisdict['branch']=clean_string(t.find("p",{"class":"pv-entity__secondary-title pv-entity__fos t-14 t-black t-normal"}).find("span",{"class":"pv-entity__comma-item"}).text) if t.find("p",{"class":"pv-entity__secondary-title pv-entity__fos t-14 t-black t-normal"}).find("span",{"class":"pv-entity__comma-item"}).find(text=re.compile("Computer Science",re.IGNORECASE)) else '',
-                    thisdict['branch']=clean_string(t.find("p",{"class":"pv-entity__secondary-title pv-entity__fos t-14 t-black t-normal"}).find("span",{"class":"pv-entity__comma-item"}).text) if t.find("p",{"class":"pv-entity__secondary-title pv-entity__fos t-14 t-black t-normal"}).find("span",{"class":"pv-entity__comma-item"}).find(text=re.compile("Computer Science",re.IGNORECASE)) else ''
-                    thisdict['duration']=clean_string(t.find("p",{"class":"pv-entity__dates t-14 t-black--light t-normal"}).find("span",{"class":""}).text) if t.find("p",{"class":"pv-entity__dates t-14 t-black--light t-normal"}).find("span",{"class":""}) else ''
-                                
+    #             print("@@@@comp@@@@")
+    name = html_soup.find("li", {"class": "inline t-24 t-black t-normal break-words"})
+    if name is not None:
+        print(name.text)
+    IDs = []
+    # Defaults = {'Name':'','College': '', 'Degree': '', 'Branch': '', 'duration': '','designation': '', 'company': '','dates_employed': '', 'employ_duration': ''}
+    # thisdict = dict.fromkeys(IDs, Defaults)
+    thisdict['Name']= clean_string(name.text)
+    thisdict['Link']= clean_string(link)
+    browser.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+    # browser.execute_script("window.scrollTo(0,700)")
+    time.sleep(random.randint(1,5))
+    if (html_soup.find("li", {"class":"pv-profile-section__list-item pv-education-entity pv-profile-section__card-item ember-view"})):
+        print('section1')
+        # edusect=html_soup.find("section", {"class":"pv-profile-section education-section ember-view"})
+        for t in html_soup.find_all("li", {"class": "pv-profile-section__list-item pv-education-entity pv-profile-section__card-item ember-view"}):
+            print('university',university)
+            print('branch',branch)
+            if(t.find("h3",{"class":"pv-entity__school-name t-16 t-black t-bold"}).find(text=re.compile("Jadavpur University",re.IGNORECASE))) :
+                thisdict['college']=t.find("h3",{"class":"pv-entity__school-name t-16 t-black t-bold"}).text if t.find("h3",{"class":"pv-entity__school-name t-16 t-black t-bold"}) else ''
+                thisdict['degree']=t.find("p",{"class":"pv-entity__secondary-title pv-entity__degree-name t-14 t-black t-normal"}).find("span",{"class":"pv-entity__comma-item"}).text if t.find("p",{"class":"pv-entity__secondary-title pv-entity__degree-name t-14 t-black t-normal"}) else ''
+                # thisdict['branch']=clean_string(t.find("p",{"class":"pv-entity__secondary-title pv-entity__fos t-14 t-black t-normal"}).find("span",{"class":"pv-entity__comma-item"}).text) if t.find("p",{"class":"pv-entity__secondary-title pv-entity__fos t-14 t-black t-normal"}).find("span",{"class":"pv-entity__comma-item"}).find(text=re.compile("Computer Science",re.IGNORECASE)) else '',
+                thisdict['branch']=clean_string(t.find("p",{"class":"pv-entity__secondary-title pv-entity__fos t-14 t-black t-normal"}).find("span",{"class":"pv-entity__comma-item"}).text) if t.find("p",{"class":"pv-entity__secondary-title pv-entity__fos t-14 t-black t-normal"}).find("span",{"class":"pv-entity__comma-item"}).find(text=re.compile("Computer Science",re.IGNORECASE)) else ''
+                thisdict['duration']=clean_string(t.find("p",{"class":"pv-entity__dates t-14 t-black--light t-normal"}).find("span",{"class":""}).text) if t.find("p",{"class":"pv-entity__dates t-14 t-black--light t-normal"}).find("span",{"class":""}) else ''
+                            
         # for x, y in thisdict.items():
         #     print(x,y)
         # IDs = []
@@ -156,7 +161,7 @@ def visitProfile(link):
         # expdict = dict.fromkeys(IDs, Defaults)
         i=0
 
-        if(html_soup.find("section", {"class":"pv-profile-section experience-section ember-view"})):
+        if (html_soup.find("section", {"class":"pv-profile-section experience-section ember-view"})):
             print("@@@@here@@@@@")
             for k in html_soup.find_all("div",{"class":"pv-entity__summary-info pv-entity__summary-info--background-section mb2"},limit=2) :
                     print("####there@@@@@")
@@ -165,22 +170,19 @@ def visitProfile(link):
                     thisdict['employ_duration'] =clean_string(k.find("span",{"class":"pv-entity__bullet-item-v2"}).text) if k.find("span",{"class":"pv-entity__bullet-item-v2"}) else ''
                     thisdict['dates_employed'] =clean_string(k.find("h4",{"class":"pv-entity__date-range t-14 t-black--light t-normal"}).find("span",{"class":""}).text) if k.find("h4",{"class":"pv-entity__date-range t-14 t-black--light t-normal"}) else ''
                                     # i=i+1                  
-                        # for x, y in thisdict.items():
-                        #     print(x,y)  
-                        # return thisdict                  
-                #         else :
-                #             thisdict=''
+            # for x, y in thisdict.items():
+            #     print(x,y)  
+            # return thisdict                  
+    #         else :
+    #             thisdict=''
 
-                #             # 'Edu':thisdict,
+    #             # 'Edu':thisdict,
 
-                # else :
-                #         thisdict=''
-                        
-        for x, y in thisdict.items():
-                        print(x,y)
-    except Exception as e :
-            thisdict=''
-            print('in except',e)
+    # else :
+    #         thisdict=''
+                                    
+    for x, y in thisdict.items():
+                    print(x,y)
     return thisdict 
 #%%
 # from selenium.webdriver.common.action_chains import ActionChains
@@ -188,9 +190,28 @@ def visitProfile(link):
 link='https://www.linkedin.com/in/chandreyee-chowdhury-56244263/'
 # link='https://www.linkedin.com/in/arjun-murmu-82073b159/'
 Recdict=visitProfile(link)
-for x, y in Recdict.items():
-    print(x,y)        
-
+if  Recdict is not None:
+    for x, y in Recdict.items():
+        print(x,y) 
+#%%
+values=Recdict.values()
+values_list = list(values);
+print(values_list)
+import pymongo
+import itertools
+import bson
+from bson import BSON
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["mydatabase"]
+mycol = mydb["alumni"]
+mycol.delete_many({})
+x = mycol.insert_many(values_list)
+print('ids of inserted documents\n---------------------')
+for id in x.inserted_ids:
+	print(id)
+for doc in mycol.find():
+    #Print each document
+    print(doc)
 
 
 # browser.find_element()
@@ -217,11 +238,10 @@ que.send_keys(Keys.ENTER)
 # que.send_keys("jadavpur university department of information technology")
 # que.send_keys(Keys.ENTER)
 profilesQueued = []
-
 profilesID = []
 page = 0
 index = 0
-limit = 10
+limit = 50
 ids1 = []
 # Defaults1 = {'Name': '', 'Edu': '', 'Exp': ''}
 IDs = []
@@ -230,7 +250,7 @@ Defaults1 = {'Name':'','Link':'','College': '', 'Degree': '', 'Branch': '', 'dur
 Recdict = dict.fromkeys(ids1, Defaults1)
 baselink = browser.current_url.partition('page')[0]
 print('@@@@@@@@@@@@@baselink@@@@@@@@@@@@@@@@@@',baselink)
-while(page <3):
+while(page <10):
     if page==0:
         nextlink=baselink
         print('page 0 nextlink:'+nextlink)
@@ -285,10 +305,24 @@ print (len(Recdict))
 for x, y in Recdict.items():
     print(x, y)  
 #%%
-print (len(Recdict))
-# print(Recdict)
-for x, y in Recdict.items():
-    print(x, y)  
+values=Recdict.values()
+values_list = list(values);
+print(values_list)
+import pymongo
+import itertools
+import bson
+from bson import BSON
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["mydatabase"]
+mycol = mydb["alumni"]
+mycol.delete_many({})
+x = mycol.insert_many(values_list)
+print('ids of inserted documents\n---------------------')
+for id in x.inserted_ids:
+	print(id)
+for doc in mycol.find():
+    #Print each document
+    print(doc)
 
 #%%
  #print only values having non null
@@ -298,9 +332,7 @@ for x, y in recdict1.items():
     print(x,y) 
 print (len(Recdict))
 # print(Recdict)
-values=Recdict.values()
-values_list = list(values);
-print(values_list)
+v
 
     #%%
 import bson
@@ -341,22 +373,7 @@ print(list(df.columns))
 # print(df.Name.to_string(index=False))
 
 # %%
-import pymongo
-import itertools
-import bson
-from bson import BSON
-myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-mydb = myclient["mydatabase"]
-mycol = mydb["alumni"]
-mycol.delete_many({})
-x = mycol.insert_many(values_list)
-print('ids of inserted documents\n---------------------')
-for id in x.inserted_ids:
-	print(id)
-#%%
-for doc in mycol.find():
-    #Print each document
-    print(doc)
+
 # %% 
 print(z)
 myquery = {"Name": "Chandreyee Chowdhury" }  
